25db9178c87d942a70158b949ebabf8eed276219
JLS93 <jlsvbn@gmail.com>
2022-06-21 10:10:02 +0200

    Add SolveLinear function

---
+++ Functions/SolveLinear.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SuperBASIC.Functions
+{
+    class SolveLinear : IFunction
+    {
+        public float Apply(List<BasicNumber> arguments)
+        {      
+
+            if (arguments[0] == 0 && arguments[1] != 0)
+            {
+                throw new NullReferenceException();               
+            }
+            else if (arguments[0] == 0 && arguments[1] == 0)
+            {
+                return float.NaN;
+            }
+            else
+            {
+                return -arguments[1] / arguments[0];
+            }
+        }
+    }
+}
